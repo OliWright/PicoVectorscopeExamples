4d0456186585a1148f415e666a8f763796b36b3e
Oli Wright <owright@nvidia.com>
2022-11-01 08:54:43 +0000

    Big tidy-up for Starfield demo

--- src/starfield.cpp
+++ src/starfield.cpp
@@ -1,136 +1,109 @@
+// Classic Starfield demo, using the PicoVectorscope framework.
+//
+// Copyright (C) 2022 Oli Wright
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// A copy of the GNU General Public License can be found in the file
+// LICENSE.txt in the root of this project.
+// If not, see <https://www.gnu.org/licenses/>.
+//
+// oli.wright.github@gmail.com
+
 #include "picovectorscope.h"
 
 static constexpr uint32_t kNumStars = 1000;
-typedef FixedPoint<8, 7, int16_t, int32_t, false> StarCoordScalar;
+
+// Fixed-point type for storing the coordinates of stars.  16-bit.
+typedef FixedPoint<8, 7, int16_t, int32_t, false> CompactStarCoordScalar;
 struct StarCoord
 {
-    StarCoordScalar x, y, z;
+    CompactStarCoordScalar x, y, z;
 };
+
+// Fixed-point type for performing most of our intermediate maths operations. 32-bit.
 typedef FixedPoint<8, 18, int32_t, int32_t, false> StarCoordIntermediate;
 
-constexpr float kProjFloat = 0.25f;
-constexpr float kNearZFloat = 32.f;
+constexpr float kProjFloat  = 0.25f; //< Controls the perspective projection
+constexpr float kNearZFloat = 32.f; //< How near do stars get before they wrap
 
-constexpr StarCoordIntermediate kProj = StarCoordIntermediate(kProjFloat);
-constexpr StarCoordIntermediate kZOffset = StarCoordIntermediate(kNearZFloat);
-constexpr StarCoordIntermediate kNearZ = StarCoordIntermediate(kNearZFloat);
-constexpr StarCoordIntermediate kFarZ = kNearZ + StarCoordScalar::kMax;
-constexpr StarCoordIntermediate kRecipNearZ = kNearZ.recip();
+// Some derived constants in our fixed-point format
+constexpr StarCoordIntermediate kProj      = kProjFloat;
+constexpr StarCoordIntermediate kZOffset   = kNearZFloat;
+constexpr StarCoordIntermediate kNearZ     = kNearZFloat;
+constexpr StarCoordIntermediate kFarZ      = kNearZ + CompactStarCoordScalar::kMax;
 constexpr StarCoordIntermediate kRecipFarZ = kFarZ.recip();
-
-#if 1
-constexpr float nearZFloat = (float) kNearZ;
-constexpr float farZFloat = (float) kFarZ;
-
-constexpr float recipNearZFloat = (float) kRecipNearZ;
-constexpr float idealrecipNearZFloat = 1.f / kNearZFloat;
-constexpr float projOverNearZFloat = (float) Mul<0,-4>(kProj, kRecipNearZ);
-constexpr float idealProjOverNearZFloat = kProjFloat * idealrecipNearZFloat;
-
-constexpr float recipFarZFloat = (float) kRecipFarZ;
-constexpr float idealRecipFarZFloat = 1.f / (float) kFarZ;
-constexpr float projOverFarZFloat = (float) Mul<0,-6>(kProj, kRecipFarZ);
-constexpr float idealProjOverFarZFloat = kProjFloat * idealRecipFarZFloat;
-
-constexpr StarCoord kTestStarCoord = {-107.f, 37.f, 231.2f};
-constexpr float kTestStarCoordXFloat = (float) kTestStarCoord.x;
-constexpr float kTestStarCoordYFloat = (float) kTestStarCoord.y;
-constexpr float kTestStarCoordZFloat = (float) kTestStarCoord.z;
-
-constexpr StarCoordIntermediate kTestStarRecipZ = ((StarCoordIntermediate)kTestStarCoord.z + kZOffset).recip();
-constexpr StarCoordIntermediate kTestStarRecipZ2 = Div<1>(StarCoordIntermediate(1.f), ((StarCoordIntermediate)kTestStarCoord.z + kZOffset));
-constexpr float kTestStarRecipZFloat = (float) kTestStarRecipZ;
-constexpr float kTestStarRecipZFloat2 = (float) kTestStarRecipZ2;
-constexpr float kTestStarRecipZFloat3 = (float) 1.f / (kTestStarCoordZFloat + kNearZFloat);
-constexpr StarCoordIntermediate kTestStarProjOverZ = Mul<0,-4>(kProj, kTestStarRecipZ);
-constexpr float kTestStarProjOverZFloat = (float) kTestStarProjOverZ;
-constexpr float kTestStarProjOverZFloat2 = kProjFloat * kTestStarRecipZFloat3;
-constexpr StarCoordIntermediate kTestStarScreenX = Mul<StarCoordScalar::kNumWholeBits,-4>(kTestStarCoord.x, kTestStarProjOverZ) + 0.5f;
-constexpr float kTestStarScreenXFloat = (float) kTestStarScreenX;
-constexpr float kTestStarScreenXFloat2 = kTestStarCoordXFloat * kTestStarProjOverZFloat2 + 0.5f;
-
-constexpr StarCoordIntermediate zeroBrightness = Mul<StarCoordScalar::kNumWholeBits, -4, 12>(kNearZ, kRecipFarZ);
-constexpr StarCoordIntermediate brightness = Mul<StarCoordScalar::kNumWholeBits, -4, 9>(kNearZ, kTestStarRecipZ);//< 1.0 at kNearZ
-constexpr float zeroBrightnessFloat = (float) zeroBrightness;
-constexpr float idealZeroBrightnessFloat = kNearZFloat * idealRecipFarZFloat;
-constexpr float brightnessFloat = (float) brightness;
-
-#endif
+constexpr StarCoordIntermediate kBrightnessAtFarZ
+    = Mul<CompactStarCoordScalar::kNumWholeBits, -4, 12>(kNearZ, kRecipFarZ);
 
 static StarCoord s_stars[kNumStars];
 
-static StarCoordScalar s_starSpeed(2.f);
-static LogChannel StarDetails(false);
+static CompactStarCoordScalar s_starSpeed(2.f);
 
+// Delcare the PicoVectorscope Demo
 class Starfield : public Demo
 {
 public:
     Starfield() : Demo() {}
+    void Init();
     void UpdateAndRender(DisplayList& displayList, float dt);
 };
 static Starfield s_starfield;
 
+void Starfield::Init()
+{
+    for (StarCoord& star : s_stars)
+    {
+        star.x = CompactStarCoordScalar::randFullRange();
+        star.y = CompactStarCoordScalar::randFullRange();
+        star.z = CompactStarCoordScalar((CompactStarCoordScalar::StorageType)(
+            ((CompactStarCoordScalar::StorageType)SimpleRand()) & 0x7fff));
+    }
+}
+
 void Starfield::UpdateAndRender(DisplayList& displayList, float dt)
 {
-    static bool doneInit = false;
-    if(!doneInit)
+    if (Buttons::IsJustPressed(Buttons::Id::Left))
     {
-        doneInit = true;
-        for(uint32_t i = 0; i < kNumStars; ++i)
-        {
-            StarCoord& star = s_stars[i];
-            star.x = StarCoordScalar::randFullRange();
-            star.y = StarCoordScalar::randFullRange();
-            star.z = StarCoordScalar((StarCoordScalar::StorageType) (((StarCoordScalar::StorageType) SimpleRand()) & 0x7fff));
-
-            LOG_INFO(StarDetails, "x: %f, y: %f, z; %f\n", (float) star.x, (float) star.y, (float) star.z);
-        }
+        s_starSpeed *= 0.75f;
+    }
+    if (Buttons::IsJustPressed(Buttons::Id::Right))
+    {
+        s_starSpeed *= 1.25f;
     }
 
-    if(Buttons::IsJustPressed(Buttons::Id::Left))
+    for (StarCoord& star : s_stars)
     {
-        //s_starSpeed *= 0.75f; //<--- Broken
-        //s_starSpeed = s_starSpeed * 0.75f;
-    }
-    if(Buttons::IsJustPressed(Buttons::Id::Right))
-    {
-        //s_starSpeed *= 1.25f;
-    }
-
-
-    LOG_INFO(StarDetails, "Stars update\n");
-    for(uint32_t i = 0; i < kNumStars; ++i)
-    {
-        StarCoord& star = s_stars[i];
         star.z -= s_starSpeed;
-        if(star.z < StarCoordScalar(0.f))
+        if (star.z < 0) // Wrap?
         {
-            star.z += StarCoordScalar(StarCoordScalar::kMaxStorageType);
+            star.z += CompactStarCoordScalar(CompactStarCoordScalar::kMaxStorageType);
         }
 
-        // LOG_INFO(StarDetails, "x: %f, y: %f, z; %f\n", (float) x, (float) y, (float) z);
-
-        // Careful fixed-point math to maintain precision
-        StarCoordIntermediate recipZ = (StarCoordIntermediate(star.z) + kZOffset).recip();
-        StarCoordIntermediate projOverZ = Mul<0,0>(kProj, recipZ);
-        StarCoordIntermediate screenX = Mul<-4,StarCoordScalar::kNumWholeBits>(projOverZ, star.x) + 0.5f;
-        if((screenX < StarCoordIntermediate(1.f)) && (screenX > StarCoordIntermediate(0.f)))
+        // Careful fixed-point maths to maintain precision
+        StarCoordIntermediate recipZ    = (StarCoordIntermediate(star.z) + kZOffset).recip();
+        StarCoordIntermediate projOverZ = Mul<0, 0>(kProj, recipZ);
+        StarCoordIntermediate screenX
+            = Mul<-4, CompactStarCoordScalar::kNumWholeBits>(projOverZ, star.x) + 0.5f;
+        if ((screenX < 1) && (screenX > 0))
         {
-            StarCoordIntermediate screenY = Mul<-4,StarCoordScalar::kNumWholeBits>(projOverZ, star.y) + 0.5f;
-            if((screenY < StarCoordIntermediate(1.f)) && (screenY > StarCoordIntermediate(0.f)))
+            StarCoordIntermediate screenY
+                = Mul<-4, CompactStarCoordScalar::kNumWholeBits>(projOverZ, star.y) + 0.5f;
+            if ((screenY < 1) && (screenY > 0))
             {
-                constexpr StarCoordIntermediate zeroBrightness = Mul<StarCoordScalar::kNumWholeBits, -4, 12>(kNearZ, kRecipFarZ);
-                StarCoordIntermediate brightness = Mul<StarCoordScalar::kNumWholeBits, -4, 12>(kNearZ, recipZ);//< 1.0 at kNearZ
-                brightness -= zeroBrightness;
-                if(brightness > 0.f)
-                {
-                    if(brightness > 1.f)
-                    {
-                        brightness = 1.f;
-                    }
-                    displayList.PushPoint(screenX, screenY, brightness);
-                }
-                LOG_INFO(StarDetails, "x: %f, y: %f\n", (float) screenX, (float) screenY);
+                // The star is on the screen.  Calculate a brightness, and draw it.
+                StarCoordIntermediate brightness
+                    = Mul<CompactStarCoordScalar::kNumWholeBits, -4, 12>(kNearZ, recipZ);
+                brightness -= kBrightnessAtFarZ; // Ensure brightness 0 at FarZ
+                displayList.PushPoint(screenX, screenY, brightness);
             }
         }
     }
